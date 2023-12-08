2057b064643e507c05e52c382ec8a0bc829e0cf5
Jordy Zenteno <106561804+JotaZen@users.noreply.github.com>
2023-12-07 23:27:50 -0300

    Update firestore.js

--- js/firestore.js
+++ js/firestore.js
@@ -8,7 +8,7 @@ const url = new URLSearchParams(window.location.search);
 const apiKey = await url.get('key');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBHgX4GNvN9rOcMnM6HqmsRQX5AInd2YmU",
+    apiKey: "AIzaSyBHgX4GNvN9rOcMnM6HqmsRQX5AInd2YmU", // Te la regalo usala, pero literal puedes sacar una gratis por que querrias esta xd 
     authDomain: "biblitecamusical.firebaseapp.com",
     projectId: "biblitecamusical",
     storageBucket: "biblitecamusical.appspot.com",
